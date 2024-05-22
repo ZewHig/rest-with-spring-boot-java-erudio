ce43ae346123ec2c676accf28f43c53e04ab0ca5
ZewHig <matheus.donato1115@gmail.com>
2024-05-22 14:52:03 -0300

    Fist Step Spring

---
+++ rest-with-spring-boot-java-erudio/src/test/java/br/com/erudio/StartUp.java
@@ -0,0 +1,13 @@
+package br.com.erudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartUp {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
